127bc97e0f8328da7d22b5f5b006fb7a856d2465
Tony Yip <tony@opensource.hk>
2016-11-30 11:25:51 +0800

    Add card outter shell

---
+++ react/Card.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+import classnames from 'classnames';
+
+export default class Card extends React.Component {
+  render() {
+    const classes = classnames('card', this.props.size || '');
+    return <div className={classes}>{this.props.children}</div>;
+  }
+}
---
+++ vue/Card.vue
@@ -0,0 +1,15 @@
+<template>
+    <div :class="['card', size]">
+        <slot />
+    </div>
+</template>
+<script>
+  export default{
+    props: {
+      size: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
