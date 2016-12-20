d64302291d305139e8109a19ad5daee4a7f43c01
Tony Yip <tony@opensource.hk>
2016-12-20 23:15:00 +0800

    Create config for different version

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 'use strict';
 
-module.exports = {
+const UglifyPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+const OccurenceOrderPlugin = require('webpack/lib/optimize/OccurrenceOrderPlugin');
+const DedupePlugin = require('webpack/lib/optimize/DedupePlugin');
+const HotModuleReplacementPlugin = require('webpack/lib/HotModuleReplacementPlugin');
+const AggressiveMergingPlugin = require('webpack/lib/optimize/AggressiveMergingPlugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+const merge = require('webpack-merge');
+
+const base = {
+  plugins: [
+    new OccurenceOrderPlugin(),
+    new DedupePlugin(),
+    new AggressiveMergingPlugin(),
+    new StyleLintPlugin()
+  ],
   devtool: 'source-map',
   entry: {
-    vue: [
-      'webpack-dev-server/client?http://localhost:8080/',
-      './vue'
-    ],
-    react: [
-      'webpack-dev-server/client?http://localhost:8080/',
-      './react'
-    ]
   },
   output: {
     path: `${__dirname}/public`,
@@ -18,19 +25,33 @@ module.exports = {
     filename: '[name].js'
   },
   resolve: {
-    extension: ['', '.vue', '.js'],
+    extension: ['', '.jsx', '.vue', '.js'],
     fallback: ['node_modules']
   },
   module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'eslint'
+      }
+    ],
     loaders: [
       {
         test: /\.vue$/,
         loader: 'vue'
       },
       {
+        test: /\.jsx$/,
+        loader: ['babel']
+      },
+      {
         test: /\.js$/,
-        loader: 'babel',
-        include: `${__dirname}/react`
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        loader: ['scss', 'css']
       }
     ]
   },
@@ -38,5 +59,33 @@ module.exports = {
     loaders: {
       js: 'babel'
     }
+  },
+  eslint: {
+    configFile: './.eslintrc.yml'
   }
+};
+
+const development = merge.smart({
+  plugins: [
+    new HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: ['react-hot']
+      }
+    ]
+
+  }
+}, base);
+
+const production = merge.smart({
+  plugins: [
+    new UglifyPlugin({minimize: true})
+  ]
+}, base);
+
+module.exports = {
+  base, development, production
 };
